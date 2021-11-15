9d80ec3677319b5e6e196fd94f5c2b956e855b96
zhuoxiao yang <91831716+yangtianwang@users.noreply.github.com>
2021-11-15 12:36:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   int n,i=i,b=1,a=0,c;
+   cin>>n;
+   
+   cout<<b<<endl;
+   for(int i - 0;i < n; i++) {
+      c=a+b;
+      cout<<c<<endl;
+      a=b;
+      b=c;
+      
+   }
    return 0;
 }
